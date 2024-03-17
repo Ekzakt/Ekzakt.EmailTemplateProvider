a5d5a65cbb09b0ddaff5273f5e7d51964d3b5bcf
EricJansen <EricJansen@LAPTOP-EJ>
2024-03-17 14:07:17 +0100

    Using Ekzakt.EmailSender.Core.Models namespace instead.

--- Ekzakt.EmailTemplateProvider.Core/Models/EmailTemplate.cs
+++ Ekzakt.EmailTemplateProvider.Core/Models/EmailTemplate.cs
@@ -7,17 +7,7 @@ public class EmailTemplate
 {
     public string RecipientType { get; set; } = string.Empty;
 
-    public EmailAddress? From { get; set; }
-
-    public List<EmailAddress> Tos { get; set; } = new();
-
-    public List<EmailAddress>? Ccs { get; set; } = new();
-
-    public List<EmailAddress>? Bccs { get; set; } = new();
-
-    public string Subject { get; set; } = string.Empty;
-
-    public TemplateBody Body { get; set; } = new();
+    public Email? 
 
 
     [JsonIgnore]
--- Ekzakt.EmailTemplateProvider.Core/Models/TemplateBody.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Ekzakt.EmailTemplateProvider.Core.Models;
-
-public class TemplateBody
-{
-    public string Html { get; set; } = string.Empty;
-    public string? Text { get; set; }
-}
