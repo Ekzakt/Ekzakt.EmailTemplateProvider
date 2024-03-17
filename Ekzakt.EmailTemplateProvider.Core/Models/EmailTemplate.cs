5d5366b57afb1304f46dfb2ecfefd7ac108c7805
EricJansen <EricJansen@LAPTOP-EJ>
2024-03-17 15:09:54 +0100

    Fixed merge conflicts.

--- Ekzakt.EmailTemplateProvider.Core/Models/EmailTemplate.cs
+++ Ekzakt.EmailTemplateProvider.Core/Models/EmailTemplate.cs
@@ -8,36 +8,6 @@ public class EmailTemplate : Email
 {
     public string RecipientType { get; set; } = string.Empty;
 
-    public List<EmailAddress> Tos { get; set; } = new();
-
-    public List<EmailAddress>? Ccs { get; set; } = new();
-
-    public List<EmailAddress>? Bccs { get; set; } = new();
-
-    public string Subject { get; set; } = string.Empty;
-
-    public TemplateBody Body { get; set; } = new();
-
-    public List<EmailAddress> Tos { get; set; } = new();
-
-    public List<EmailAddress>? Ccs { get; set; } = new();
-
-    public List<EmailAddress>? Bccs { get; set; } = new();
-
-    public string Subject { get; set; } = string.Empty;
-
-    public TemplateBody Body { get; set; } = new();
-
-    public List<EmailAddress> Tos { get; set; } = new();
-
-    public List<EmailAddress>? Ccs { get; set; } = new();
-
-    public List<EmailAddress>? Bccs { get; set; } = new();
-
-    public string Subject { get; set; } = string.Empty;
-
-    public TemplateBody Body { get; set; } = new();
-
 
     [JsonIgnore]
     public bool IsAdmin => RecipientType == RecipientTypes.USER;
