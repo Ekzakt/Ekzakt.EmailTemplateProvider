1d1407bec1d8e9c7d1dfd92692d8c5a0a2e3227f
EricJansen <EricJansen@LAPTOP-EJ>
2024-03-29 15:58:03 +0100

    Created developer branch.

---
+++ Ekzakt.EmailTemplateProvider.Core/Caching/CacheKeyNotFoundEventArgs.cs
@@ -0,0 +1,10 @@
+﻿namespace Ekzakt.EmailTemplateProvider.Core.Caching;
+
+public class CacheKeyNotFoundEventArgs : EventArgs
+{
+    public string? TenantId { get; set; }
+
+    public string CultureName { get; set; } = string.Empty;
+
+    public string TemplateName { get; set;} = string.Empty;
+}
---
+++ Ekzakt.EmailTemplateProvider.Core/Caching/IEmailTemplatesCache.cs
@@ -0,0 +1,15 @@
+﻿using Ekzakt.EmailTemplateProvider.Core.Models;
+using Ekzakt.EmailTemplateProvider.Core.Requests;
+
+namespace Ekzakt.EmailTemplateProvider.Core.Caching;
+
+public interface IEmailTemplatesCache
+{
+    delegate Task AsyncEventHandler<TEventArgs>(TEventArgs e);
+
+    event AsyncEventHandler<CacheKeyNotFoundEventArgs> CacheKeyNotFound;
+
+    Task<EmailTemplateSettings?> GetTemplateAsync(EmailTemplatesRequest request, string? fallbackCultureName = null);
+
+    void SetTemplate(EmailTemplateSettings emailTemplateSettings);
+}
