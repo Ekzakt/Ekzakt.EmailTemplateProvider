cf9037321db9895ab3ed0ac7655413a1c5b69240
EricJansen <EricJansen@LAPTOP-EJ>
2024-04-03 20:07:10 +0200

    Added ICloanble to EmailInfo.

--- Ekzakt.EmailTemplateProvider.Core/Models/EmailInfo.cs
+++ Ekzakt.EmailTemplateProvider.Core/Models/EmailInfo.cs
@@ -2,9 +2,18 @@
 
 namespace Ekzakt.EmailTemplateProvider.Core.Models;
 
-public class EmailInfo
+public class EmailInfo : ICloneable
 {
     public string RecipientType { get; set; } = string.Empty;
 
     public Email? Email { get; set; }
+
+    public object Clone()
+    {
+        EmailInfo cloned = (EmailInfo)MemberwiseClone();
+        cloned.RecipientType = RecipientType;
+        cloned.Email = Email;
+
+        return cloned;
+    }
 }
